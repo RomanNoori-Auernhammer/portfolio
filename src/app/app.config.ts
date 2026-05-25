690f355786b2f39cab546a563649b1357c1765f9
Roman Noori-Auernhammer <r.noori.auernhammer@gmail.com>
2026-05-25 13:40:00 +0200

    fix: add translation initialization in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, HttpClient } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { firstValueFrom } from 'rxjs';
 import { routes } from './app.routes';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+function initTranslations(translate: TranslateService) {
+  return () => firstValueFrom(translate.use(translate.getDefaultLang() ?? 'de'));
 }
 
 export const appConfig: ApplicationConfig = {
@@ -28,5 +33,11 @@ export const appConfig: ApplicationConfig = {
         loader: { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient] },
       }),
     ),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initTranslations,
+      deps: [TranslateService],
+      multi: true,
+    },
   ],
 };
